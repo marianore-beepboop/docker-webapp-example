cbfeb24513088c5def7928f4e5041353f10c4227
marianore-beepboop <marianoreinfo@gmail.com>
2021-06-11 14:44:21 -0300

    First commit, not working

---
+++ api/src/wsgi.py
@@ -0,0 +1,9 @@
+"""Application entry point."""
+#from .webapp import create_app
+
+from webapp import create_app
+
+app = create_app()
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", debug=True)
